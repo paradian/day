b5b7b4b601c64303b4d1663ccb2c3b2e2fc6cca7
paradian <helloparadian@gmail.com>
2018-09-17 17:42:48 +0800

    'commitSecondHtml'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 var container = document.getElementsByClassName('container')[0]
 container.addEventListener('click',(e) => {
-    console.log(container.style)
-    container.style.backgroundColor = 'blue';
+    console.log(container.style,'use .')
+   console.log(container.getAttribute('style'),'use getAttribute') 
+   console.log(container.getAttribute,'hello')
+   container.setAttribute('style','')
+    setTimeout(() => {
+        container.style.backgroundColor = 'blue'
+        console.log(container.getAttribute('style'))
+        } ,3000)
+    
 })
