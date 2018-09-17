2054cee86e018ec2982f4284e0be1bc715322e69
paradian <helloparadian@gmail.com>
2018-09-17 11:25:32 +0800

    'delete'

--- js/index.js
+++ js/index.js
@@ -2,6 +2,4 @@ var container = document.getElementsByClassName('container')[0]
 container.addEventListener('click',(e) => {
     console.log(container.style)
     container.style.backgroundColor = 'blue';
-    container.style.transition = '1s'
-    container.style.transform = 'scale(2)'
 })
